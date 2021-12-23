c84e1c04476dcde48d5a8835508238c0ce926ee5
John Gardiner Myers <jgmyers@proofpoint.com>
2021-12-23 13:32:45 -0800

    Bump version

--- version.go
+++ version.go
@@ -21,7 +21,7 @@ var Version = KOPS_RELEASE_VERSION
 
 // These constants are parsed by build tooling - be careful about changing the formats
 const (
-	KOPS_RELEASE_VERSION = "1.24.0-alpha.6"
+	KOPS_RELEASE_VERSION = "1.24.0-alpha.7"
 )
 
 // GitVersion should be replaced by the makefile
